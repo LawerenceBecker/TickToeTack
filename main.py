12e28ddaf9a72e4ae3168fd765590a1103144ee2
LawerenceBecker <95592713+LawerenceBecker@users.noreply.github.com>
2022-03-29 07:54:14 -0600

    Inital commit

---
+++ main.py
@@ -0,0 +1,9 @@
+### Import info ###
+
+## Board is a hashmap
+
+### Ideas ###
+
+## Reads the move and plays the best move based on that
+## Tries to play a set moveset based on the first move
+## Use a point system to way what choice it should make
